66288d1539aa35446bee6db323884422851eb45d
LuaiObaid <Luainesta13@hotmail.com>
2025-03-22 16:07:23 +0100

    responsive components

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { CustomerReview, Footer, Hero, Nav, PopularProducts, Services, SpecialOffer, Subscribers, SuperQuality } from "./sections";
+const App = () => (
+  <main className="relative">
+    <Nav/>    
+    <section className="xl:sm:px-16 xl:px-8 xl:sm:py-24 xl:py-12 wide:sm:pr-16 wide:pr-8 sm:pb-24 pb-12">
+      <Hero/>
+    </section>
+    <section className="sm:px-16 px-8 sm:py-24 py-12">
+      <PopularProducts/>
+    </section>
+    <section className="sm:px-16 px-8 sm:py-24 py-12">
+      <SuperQuality/>
+    </section>
+    <section className="sm:px-16 px-8 py-10">
+      <Services/>
+    </section>
+    <section className="sm:px-16 px-8 sm:py-24 py-12">
+      <SpecialOffer/>
+    </section>
+    <section className="bg-pale-blue sm:px-16 px-8 sm:py-24 py-12">
+      <CustomerReview/>
+    </section>
+    <section className="sm:px-16 px-8 sm:py-32 py-16 w-full">
+      <Subscribers/>
+    </section>
+    <section className="sm:px-16 px-8 sm:py-24 py-12 bg-black">
+      <Footer/>
+    </section>
+  </main>
+);
+
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import CustomerReview from "./CustomerReview";
+import Footer from "./Footer";
+import Nav from "../components/Nav";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribers from "./Subscribers";
+import SuperQuality from "./SuperQuality";
+
+
+export{
+    Hero, 
+    PopularProducts, 
+    Subscribers,
+    SuperQuality, 
+    CustomerReview,
+    Footer,
+    Nav,
+    Services,
+    SpecialOffer
+}
